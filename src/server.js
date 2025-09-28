3430f2cfd58f21b084476785a915730968c6bd1a
quyen <quyen2909a@gmail.com>
2025-09-28 15:58:08 +0700

    module auth

--- src/server.js
+++ src/server.js
@@ -1,8 +1,8 @@
 require("dotenv").config();
 const express = require("express");
-const mongoose = require("mongoose");
 const cookieParser = require("cookie-parser");
 const connectDB = require("./configs/database");
+const cors = require("cors");
 
 
 const authRoutes = require("./routes/auth.routes");
@@ -11,6 +11,18 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 
+const FRONTEND_ORIGIN = process.env.FRONTEND_ORIGIN || "http://localhost:5173";
+app.use(
+  cors({
+    origin: FRONTEND_ORIGIN, // phải khớp domain FE
+    credentials: true, // bắt buộc nếu dùng cookie httpOnly
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+
+// test nhanh
+app.get("/api/ping", (req, res) => res.json({ ok: true }));
 // Routes
 app.use("/api/auth", authRoutes);
 
